bb13851e12fc26c9f5dc0d65c191f8bf90700612
antras.lofra <javierantraslopez@gmail.com>
2025-04-24 20:07:38 +0200

    Segundo error. Si no habían notas, se dividía por 0.

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -7,10 +7,14 @@ public class GestorEstudiantes {
     // Calcula la nota media de un estudiante
     public static double calcularNotaMedia(Estudiante estudiante) {
         double suma = 0;
-        for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
+        for (int i = 0; i < estudiante.getNotas().length; i++) {
             suma += estudiante.getNotas()[i];
         }
-        return suma / estudiante.getNotas().length; // Error si el array está vacío
+        double media = suma / estudiante.getNotas().length;
+        if (estudiante.getNotas().length == 0) {
+            media = 0;
+        }
+         return media;
     }
 
     // Encuentra al estudiante con la mejor nota media
